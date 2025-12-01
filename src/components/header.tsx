d2785d47482b4715c407be9c6aae7bc509576856
bastien-winant <armand.winant@gmail.com>
2025-12-01 17:56:20 +0100

    dynamic header nav link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,12 @@
 import { HStack, Stack, Heading, Text, LinkBox, LinkOverlay } from "@chakra-ui/react"
-import { NavLink } from "react-router"
+import { NavLink, useLocation } from "react-router"
 
 export default function Header() {
+	const location = useLocation()
+
+	const dynamicTarget =
+		location.pathname.startsWith("/about") ? "/" : "/about"
+
 	return (
 		<HStack
 			align="flex-start"
@@ -14,9 +19,11 @@ export default function Header() {
 				<Text textStyle={{ base: "xl", md: "2xl" }} color="fg.muted">Data Engineer</Text>
 			</Stack>
 			<LinkBox>
-				<Text textStyle={{ base: "xl", md: "2xl" }} fontWeight="semibold">About</Text>
+				<Text textStyle={{ base: "xl", md: "2xl" }} fontWeight="semibold">
+					{dynamicTarget === "/about" ? "About" : "Back"}
+				</Text>
 				<LinkOverlay asChild>
-					<NavLink to="about" />
+					<NavLink to={dynamicTarget} />
 				</LinkOverlay>
 			</LinkBox>
 		</HStack>
