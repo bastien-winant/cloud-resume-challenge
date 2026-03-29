8564b7ef7ffde0c7d27ebd4aa5f34117111eb2d0
bastien-winant <armand.winant@gmail.com>
2026-03-29 16:36:03 +0200

    add certifcation badges

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -13,7 +13,10 @@ export default function Index() {
 				this is the education section
 			</Section>
 			<Section title="certifications">
-				this is the certifications section
+				<div className="flex gap-3">
+					<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="b12c39df-bd59-4a65-a256-968c082404ee" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+					<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="25296c31-3e3e-4f3b-abdc-7c178d466ed1" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+				</div>
 			</Section>
 		</div>
 	)
