86fc75a6741dec353a0db8b1105074cbc5f38697
bastien-winant <armand.winant@gmail.com>
2026-04-13 22:51:10 +0200

    aside link hover

--- front-end/src/components/Aside.tsx
+++ front-end/src/components/Aside.tsx
@@ -8,17 +8,17 @@ export default function Aside() {
 			<nav className="flex flex-col text-xs">
 				<a
 					href="mailto:bastien.winant@gmail.com"
-					className="px-1.5 py-3 border-t border-stone-400/20"
+					className="px-1.5 py-3 border-t border-stone-400/20 hover:bg-stone-50 hover:text-stone-950"
 				>hello@figma.com</a>
 				<a
 					href="https://github.com/bastien-winant"
 					target="_blank"
-					className="px-1.5 py-3 border-t border-stone-400/20"
+					className="px-1.5 py-3 border-t border-stone-400/20 hover:bg-stone-50 hover:text-stone-950"
 				>(555) 123-4567</a>
 				<a
 					href="https://www.xing.com/profile/Bastien_Winant"
 					target="_blank"
-					className="px-1.5 py-3 border-t border-stone-400/20"
+					className="px-1.5 py-3 border-t border-stone-400/20 hover:bg-stone-50 hover:text-stone-950"
 				>Linkedin</a>
 			</nav>
 		</aside>
