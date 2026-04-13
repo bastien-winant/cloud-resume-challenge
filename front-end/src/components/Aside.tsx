38513cd1c5742591ed8f3a661d7f0ef1d587b857
bastien-winant <armand.winant@gmail.com>
2026-04-13 22:35:40 +0200

    responsive design

--- front-end/src/components/Aside.tsx
+++ front-end/src/components/Aside.tsx
@@ -1,7 +1,7 @@
 export default function Aside() {
 	return (
-		<aside className="self-start md:sticky top-0 flex flex-col gap-20 border-t border-stone-400/40 py-7 mx-3">
-			<p className="w-[85%]">
+		<aside className="self-start md:sticky top-0 flex flex-col gap-20 border-t border-stone-400/40 py-7 mx-3 md:mx-0">
+			<p className="w-[85%] max-w-100">
 				A seasoned Senior Producer with 10+ years of experience,
 				I excel in leading complex marketing and design projects from concept to completion.
 			</p>
--- front-end/src/routes/root.tsx
+++ front-end/src/routes/root.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge"
 export default function Root() {
 	return (
 		<div className={twMerge(
-			"min-h-dvh grid auto-rows-min grid-cols-1 md:grid-cols-[360px_1fr] p-5",
-			"bg-stone-950 text-stone-100 text-base font-light"
+			"min-h-dvh grid auto-rows-min grid-cols-1 md:grid-cols-[360px_1fr] gap-x-10 p-5 md:px-7",
+			"text-stone-100 text-base font-light"
 		)}>
 			<Header />
 			<Aside />
