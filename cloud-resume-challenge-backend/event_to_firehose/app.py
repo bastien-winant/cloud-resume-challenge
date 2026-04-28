12690b8f79d851a711d406265a5c56a55c988aca
bastien-winant <armand.winant@gmail.com>
2026-04-28 11:06:53 +0200

    s3 to dynamo lambda config

--- cloud-resume-challenge-backend/event_to_firehose/app.py
+++ cloud-resume-challenge-backend/event_to_firehose/app.py
@@ -6,46 +6,47 @@ from datetime import datetime, timedelta
 import backoff
 import boto3
 
-delivery_stream_name = "ENTER YOUR DELIVERY STREAM NAME HERE"
+delivery_stream_name = "cloud-resume-visitor-log-delivery-stream"
 region = "eu-central-1"
-firehose = boto3.client("firehose", region_name=region)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+firehose = boto3.client("firehose", region_name=region)
+
 @backoff.on_exception(backoff.expo, Exception, max_tries=5, jitter=backoff.full_jitter)
-def put_record(record: dict):
+def put_record(record: dict) -> dict:
 	"""
 	Put individual records to Firehose with backoff and retry.
 
 	Args:
-			record (dict): The data record to be sent to Firehose.
+		record (dict): The data record to be sent to Firehose.
 
 	This method attempts to send an individual record to the Firehose delivery stream.
 	It retries with exponential backoff in case of exceptions.
 	"""
 	try:
-		entry = _create_record_entry(record)
-		response = firehose.put_record(DeliveryStreamName=delivery_stream_name, Record=entry)
+		entry: dict = _create_record_entry(record)
+		response: dict = firehose.put_record(DeliveryStreamName=delivery_stream_name, Record=entry)
 		_log_response(response, entry)
+		return { 'response': response }
 	except Exception:
 		logger.info(f"Fail record: {record}.")
 		raise
-
 
 def _create_record_entry(record: dict) -> dict:
 	"""
 	Create a record entry for Firehose.
 
 	Args:
-			record (dict): The data record to be sent.
+		record (dict): The data record to be sent.
 
 	Returns:
-			dict: The record entry formatted for Firehose.
+		dict: The record entry formatted for Firehose.
 
 	Raises:
-			Exception: If a simulated network error occurs.
+		Exception: If a simulated network error occurs.
 	"""
 	if random.random() < 0.2:
 		raise Exception("Simulated network error")
@@ -59,16 +60,15 @@ def _log_response(response: dict, entry: dict):
 	Log the response from Firehose.
 
 	Args:
-			response (dict): The response from the Firehose put_record API call.
-			entry (dict): The record entry that was sent.
+		response (dict): The response from the Firehose put_record API call.
+		entry (dict): The record entry that was sent.
 	"""
 	if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
 		logger.info(f"Sent record: {entry}")
 	else:
 		logger.info(f"Fail record: {entry}")
 
+
 def lambda_handler(event, context):
-	try:
-		put_record(event)
-	except Exception as e:
-		raise e
+	result = put_record(event)
+	return result
--- cloud-resume-challenge-backend/s3_to_dynamo/app.py
+++ cloud-resume-challenge-backend/s3_to_dynamo/app.py
@@ -1,27 +1,29 @@
 import json
-import logging
 import urllib.parse
 import boto3
 
 s3 = boto3.client('s3')
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
+dynamodb = boto3.client('dynamodb')
+table = dynamodb.Table('cloud-resume-visitor-logs')
 
 def lambda_handler(event, context):
+	print("Received event: " + json.dumps(event, indent=2))
+
 	# Get the object from the event and show its content type
 	bucket = event['Records'][0]['s3']['bucket']['name']
 	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
 	try:
 		response = s3.get_object(Bucket=bucket, Key=key)
 
 		body = response['Body'].read().decode('utf-8')
-		print("CONTENT BODY: " + body)
+		print("Body content type: ", type(body))
 
+		print("CONTENT TYPE: " + response['ContentType'])
 		return response['ContentType']
 	except Exception as e:
 		logger.info(
-			f'Error getting object {key} from bucket {bucket}. Make sure they exist and your bucket is in the same region as this function.')
+			f'''Error getting object {key} from bucket {bucket}.
+			Make sure they exist and your bucket is in the same region as this function.''')
 		raise e
