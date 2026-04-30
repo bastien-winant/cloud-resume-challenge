8bd5d511df91a8bd69685de0211e4a2794ae22f4
bastien-winant <armand.winant@gmail.com>
2026-04-30 15:30:47 +0200

    update template

--- back-end/event_to_firehose/app.py
+++ back-end/event_to_firehose/app.py
@@ -7,120 +7,100 @@ import backoff
 import boto3
 
 class Config:
-  def __init__(self):
-    self.delivery_stream_name = "cloud-resume-visitor-logs-stream"
-    self.region = "eu-central-1"
+	def __init__(self):
+		self.delivery_stream_name = "cloud-resume-visitor-logs-stream"
+		self.region = "eu-central-1"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class FirehoseClient:
-  """
-  AWS Firehose client to send records and monitor metrics.
+	"""
+	AWS Firehose client to send records and monitor metrics.
 
-  Attributes:
-    config (object): Configuration object with delivery stream name and region.
-    delivery_stream_name (str): Name of the Firehose delivery stream.
-    region (str): AWS region for Firehose and CloudWatch clients.
-    firehose (boto3.client): Boto3 Firehose client.
-  """
+	Attributes:
+		config (object): Configuration object with delivery stream name and region.
+		delivery_stream_name (str): Name of the Firehose delivery stream.
+		region (str): AWS region for Firehose and CloudWatch clients.
+		firehose (boto3.client): Boto3 Firehose client.
+	"""
 
-  def __init__(self, config):
-    """
-    Initialize the FirehoseClient.
+	def __init__(self, config):
+		"""
+		Initialize the FirehoseClient.
 
-    Args:
-      config (object): Configuration object with delivery stream name and region.
-    """
-    self.config = config
-    self.delivery_stream_name = config.delivery_stream_name
-    self.region = config.region
+		Args:
+				config (object): Configuration object with delivery stream name and region.
+		"""
+		self.config = config
+		self.delivery_stream_name = config.delivery_stream_name
+		self.region = config.region
 
-    try:
-      self.firehose = boto3.client("firehose", region_name=self.region)
-    except Exception as e:
-      logger.error("Error instantiating Firehose client: ", e)
+		try:
+			self.firehose = boto3.client("firehose", region_name=self.region)
+		except Exception as e:
+			logger.info(f"Failed to instantiate the Firehose client: {e}")
 
-  @backoff.on_exception(backoff.expo, Exception, max_tries=5, jitter=backoff.full_jitter)
-  def put_record(self, record: dict):
-    """
-    Put individual records to Firehose with backoff and retry.
+	@backoff.on_exception(backoff.expo, Exception, max_tries=5, jitter=backoff.full_jitter)
+	def put_record(self, record: dict):
+		"""
+		Put individual records to Firehose with backoff and retry.
 
-    Args:
-      record (dict): The data record to be sent to Firehose.
+		Args:
+				record (dict): The data record to be sent to Firehose.
 
-    This method attempts to send an individual record to the Firehose delivery stream.
-    It retries with exponential backoff in case of exceptions.
-    """
-    try:
-      entry = self._create_record_entry(record)
-      response = self.firehose.put_record(
-          DeliveryStreamName=self.delivery_stream_name, Record=entry
-      )
-      self._log_response(response, entry)
-    except Exception:
-      logger.info(f"Fail record: {record}.")
-      raise
+		This method attempts to send an individual record to the Firehose delivery stream.
+		It retries with exponential backoff in case of exceptions.
+		"""
+		try:
+			entry = self._create_record_entry(record)
+			# response = self.firehose.put_record(DeliveryStreamName=self.delivery_stream_name, Record=entry)
+			# self._log_response(response, entry)
+			return entry
+		except Exception:
+			logger.info(f"Fail record: {record}.")
+			raise
 
-  def _create_record_entry(self, record: dict) -> dict:
-    """
-    Create a record entry for Firehose.
+	def _create_record_entry(self, record: dict) -> dict:
+		"""
+		Create a record entry for Firehose.
 
-    Args:
-      record (dict): The data record to be sent.
+		Args:
+			record (dict): The data record to be sent.
 
-    Returns:
-      dict: The record entry formatted for Firehose.
+		Returns:
+			dict: The record entry formatted for Firehose.
 
-    Raises:
-      Exception: If a simulated network error occurs.
-    """
-    if random.random() < 0.2:
-      raise Exception("Simulated network error")
-    elif random.random() < 0.1:
-      return {"Data": '{"malformed": "data"'}
+		Raises:
+			Exception: If a simulated network error occurs.
+		"""
+		if random.random() < 0.2:
+			raise Exception("Simulated network error")
+		elif random.random() < 0.1:
+			return {"Data": '{"malformed": "data"'}
 
-    return {"Data": json.dumps(record)}
+		return {"Data": json.dumps(record)}
 
-  def _log_response(self, response: dict, entry: dict):
-    """
-    Log the response from Firehose.
+	def _log_response(self, response: dict, entry: dict):
+		"""
+		Log the response from Firehose.
 
-    Args:
-      response (dict): The response from the Firehose put_record API call.
-      entry (dict): The record entry that was sent.
-    """
-    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-      logger.info(f"Sent record: {entry}")
-    else:
-      logger.info(f"Fail record: {entry}")
+		Args:
+				response (dict): The response from the Firehose put_record API call.
+				entry (dict): The record entry that was sent.
+		"""
+		if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+			logger.info(f"Sent record: {entry}")
+		else:
+			logger.info(f"Fail record: {entry}")
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+	config = Config()
+	client = FirehoseClient(config)
+	entry = client.put_record(event)
 
-    Parameters
-    ----------
-    event: dict, required
-      API Gateway Lambda Proxy Input Format
-
-      Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-      Lambda Context runtime methods and attributes
-
-      Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-      Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-      "statusCode": 200,
-      "body": json.dumps({
-        "message": "hello world",
-      }),
-    }
+	return {
+		"statusCode": 200,
+		"body": json.dumps(entry),
+	}
