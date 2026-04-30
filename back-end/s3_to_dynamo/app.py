c4c5858d7deedeaf0fc342c3af3ab1a7baeed4f3
bastien-winant <armand.winant@gmail.com>
2026-04-30 15:47:01 +0200

    use logging in s3_to_dynamo handler

--- back-end/s3_to_dynamo/app.py
+++ back-end/s3_to_dynamo/app.py
@@ -1,24 +1,26 @@
 import json
+import logging
 import urllib.parse
 import boto3
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-	# print("Received event: " + json.dumps(event, indent=2))
-
 	# Get the object from the event and show its content type
 	bucket = event['Records'][0]['s3']['bucket']['name']
 	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 	try:
 		response = s3.get_object(Bucket=bucket, Key=key)
-		print("CONTENT TYPE: " + response['ContentType'])
-		return response['ContentType']
+		body = response['Body'].read().decode('utf-8')
+		return body
 	except Exception as e:
-		print(e)
-		print(
-			'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
-				key, bucket))
+		logger.info(f'''
+			Error getting object {key} from bucket {bucket}.
+			Make sure they exist and your bucket is in the same region as this function.''')
 		raise e
 
