9ad495830853dc88c026296542fc0a5fe8ba678f
bastien-winant <armand.winant@gmail.com>
2026-04-30 14:30:18 +0200

    add parameters and conditions

---
+++ back-end/event_to_firehose/app.py
@@ -0,0 +1,126 @@
+import json
+import logging
+import random
+from datetime import datetime, timedelta
+
+import backoff
+import boto3
+
+class Config:
+  def __init__(self):
+    self.delivery_stream_name = "cloud-resume-visitor-logs-stream"
+    self.region = "eu-central-1"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class FirehoseClient:
+  """
+  AWS Firehose client to send records and monitor metrics.
+
+  Attributes:
+    config (object): Configuration object with delivery stream name and region.
+    delivery_stream_name (str): Name of the Firehose delivery stream.
+    region (str): AWS region for Firehose and CloudWatch clients.
+    firehose (boto3.client): Boto3 Firehose client.
+  """
+
+  def __init__(self, config):
+    """
+    Initialize the FirehoseClient.
+
+    Args:
+      config (object): Configuration object with delivery stream name and region.
+    """
+    self.config = config
+    self.delivery_stream_name = config.delivery_stream_name
+    self.region = config.region
+
+    try:
+      self.firehose = boto3.client("firehose", region_name=self.region)
+    except Exception as e:
+      logger.error("Error instantiating Firehose client: ", e)
+
+  @backoff.on_exception(backoff.expo, Exception, max_tries=5, jitter=backoff.full_jitter)
+  def put_record(self, record: dict):
+    """
+    Put individual records to Firehose with backoff and retry.
+
+    Args:
+      record (dict): The data record to be sent to Firehose.
+
+    This method attempts to send an individual record to the Firehose delivery stream.
+    It retries with exponential backoff in case of exceptions.
+    """
+    try:
+      entry = self._create_record_entry(record)
+      response = self.firehose.put_record(
+          DeliveryStreamName=self.delivery_stream_name, Record=entry
+      )
+      self._log_response(response, entry)
+    except Exception:
+      logger.info(f"Fail record: {record}.")
+      raise
+
+  def _create_record_entry(self, record: dict) -> dict:
+    """
+    Create a record entry for Firehose.
+
+    Args:
+      record (dict): The data record to be sent.
+
+    Returns:
+      dict: The record entry formatted for Firehose.
+
+    Raises:
+      Exception: If a simulated network error occurs.
+    """
+    if random.random() < 0.2:
+      raise Exception("Simulated network error")
+    elif random.random() < 0.1:
+      return {"Data": '{"malformed": "data"'}
+
+    return {"Data": json.dumps(record)}
+
+  def _log_response(self, response: dict, entry: dict):
+    """
+    Log the response from Firehose.
+
+    Args:
+      response (dict): The response from the Firehose put_record API call.
+      entry (dict): The record entry that was sent.
+    """
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+      logger.info(f"Sent record: {entry}")
+    else:
+      logger.info(f"Fail record: {entry}")
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+      API Gateway Lambda Proxy Input Format
+
+      Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+      Lambda Context runtime methods and attributes
+
+      Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+      Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    return {
+      "statusCode": 200,
+      "body": json.dumps({
+        "message": "hello world",
+      }),
+    }
--- back-end/s3_to_dynamo/app.py
+++ back-end/s3_to_dynamo/app.py
@@ -4,26 +4,21 @@ import boto3
 
 s3 = boto3.client('s3')
 
-dynamodb = boto3.client('dynamodb')
-table = dynamodb.Table('cloud-resume-visitor-logs')
 
 def lambda_handler(event, context):
-	print("Received event: " + json.dumps(event, indent=2))
+	# print("Received event: " + json.dumps(event, indent=2))
 
 	# Get the object from the event and show its content type
 	bucket = event['Records'][0]['s3']['bucket']['name']
 	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-
 	try:
 		response = s3.get_object(Bucket=bucket, Key=key)
-
-		body = response['Body'].read().decode('utf-8')
-		print("Body content type: ", type(body))
-
 		print("CONTENT TYPE: " + response['ContentType'])
 		return response['ContentType']
 	except Exception as e:
-		logger.info(
-			f'''Error getting object {key} from bucket {bucket}.
-			Make sure they exist and your bucket is in the same region as this function.''')
+		print(e)
+		print(
+			'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
+				key, bucket))
 		raise e
+
