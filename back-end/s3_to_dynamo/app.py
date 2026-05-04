00bed69d22b7f6806a0b8c81f794ab8678d1ddcf
bastien-winant <armand.winant@gmail.com>
2026-05-04 11:23:56 +0200

    read s3 object function

--- back-end/s3_to_dynamo/app.py
+++ back-end/s3_to_dynamo/app.py
@@ -1,28 +1,39 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
 import json
 import urllib.parse
 import boto3
+import logging
 
-print('Loading function')
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
+
+def read_s3_object():
+	# Get the object from the event and return its content
+	bucket = event['Records'][0]['s3']['bucket']['name']
+	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+	try:
+		response = s3.get_object(Bucket=bucket, Key=key)
+		print("CONTENT TYPE: " + response['ContentType'])
+
+		body = response['Body'].read().decode('utf-8')
+		print("BODY: " + body)
+
+		return body
+	except Exception as e:
+		logger.info(f'''
+			Error getting object {key} from bucket {bucket}. Make sure they exist and your bucket is in the same region as this function.''')
+		raise e
 
 
 def lambda_handler(event, context):
 	# print("Received event: " + json.dumps(event, indent=2))
 
-	# Get the object from the event and show its content type
-	bucket = event['Records'][0]['s3']['bucket']['name']
-	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 	try:
-		response = s3.get_object(Bucket=bucket, Key=key)
-		print("CONTENT TYPE: " + response['ContentType'])
-		return response['ContentType']
+		entry = read_s3_object()
 	except Exception as e:
-		print(e)
-		print(
-			'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
-				key, bucket))
 		raise e
+
 
